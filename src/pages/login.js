2daf5386d709c5ddd776995d575433fc2ee1e1c6
dadolyner <skulj.david@gmail.com>
2021-09-19 17:33:16 +0200

    testing css with pages

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,24 @@
+import '../css/login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="login-page">
+            <div class="login-header">
+                <h3>Quotastic</h3>
+                <button class="signup-button">Sign Up</button>
+            </div>
+            
+            <div class="login-body">
+                <h2>Welcome <span>back!</span></h2>
+                <p>Thank you for comming back. Hope you gave a good day and inspire others.</p>
+                
+                <p class="input-text">Email</p>
+                <input type="email" class="login-input" placeholder="Email" />
+                <p>Password</p>
+                <input type="password" class="login-input" placeholder="Password" />
+                <input type="submit" class="login-button" value="Login" />
+            </div>
+            
         </div>
     );
 }
